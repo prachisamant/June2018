480d407cba8290e3686bf0c446b681af9db575d4
prachisamant <40135771+prachisamant@users.noreply.github.com>
2018-06-12 14:42:21 -0700

    Added main method along with a test URL

--- Solution.java
+++ Solution.java
@@ -11,20 +11,34 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-/**
-* Solution is the class that accepts a url as an input parameter and make an HTTP GET request to a REST endpoint and process the output.
-*/
+
 public class Solution {
 
- 
+   /**
+     * @param args
+     */
+    public static void main(String[] args) {
+      
+    try {    
+    Solution conn = new Solution();
+      /* REST call with test url */
+      String strUrl = "https://raw.githubusercontent.com/prachisamant/June2018/master/db.json";
+      if (strUrl.trim() == null || strUrl.trim() == "") {
+          throw new RuntimeException("Please enter a valid URL.");
+        }
+      URL url = new URL(strUrl.trim());
+      
+      conn.buildRequestResponse(url);
+    }
+      catch (MalformedURLException e) {
+          System.out.println("The URL is malformed. Please enter a valid url.");
+          e.printStackTrace();
+        } 
+    }
   
-  public void buildRequestResponse(String strUrl){
+  public void buildRequestResponse(URL url){
     
     try {
-      if (strUrl.trim() == null || strUrl.trim() == "") {
-        throw new RuntimeException("Please enter a valid URL.");
-      }
-      URL url = new URL(strUrl.trim());
       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
       connection.setRequestMethod("GET");
       connection.setRequestProperty("Accept", "application/json");
@@ -43,16 +57,12 @@ public class Solution {
         stream.close();
         
         JSONParser parser = new JSONParser();
-        Long sum = 0L;
-        Long runningTotal = 0L;
+        Integer sum = 0;
+        Integer runningTotal = 0;
         String key;
         
-        // Process response for REST returning a JSON array of documents
         JSONArray jArr = (JSONArray)parser.parse(jsonLines);
-        
-        /* Below is the test JSON Array used for testing. */
-        //JSONArray jArr = (JSONArray)parser.parse("[{\"id\":1, \"name\":\"john\",\"numbers\":[3,5,7,15],\"status\":\"married\"},{\"id\":2, \"name\":\"mary\",\"numbers\":[6,8,10,2],\"status\":\"married\"},{\"id\":3, \"name\":\"julie\",\"numbers\":[10,3,8,9],\"status\":\"single\"}]");
-        
+
         System.out.println("Displaying all of the keys for each JSON document : ");
         for (int i=0; i<jArr.size(); i++) {
           JSONObject jObj = (JSONObject)jArr.get(i);
@@ -60,30 +70,26 @@ public class Solution {
           
           while(keyItr.hasNext()) {
             key = (String)keyItr.next();
-            System.out.print(key + " ");
+            System.out.print(key + " ");//Value of the key "jObj.get(key)" is not included.
             
-            //Iterate over numbers array to get sum.
+            //Iterate over numbers array of every record to get sum.
             if (key.equals("numbers")) {
               JSONArray intArr = (JSONArray)(jObj.get(key));
-              sum = 0L;
+              sum = 0;
               for (int j=0; j<intArr.size(); j++){
-                sum += ((Long)intArr.get(j)).intValue();
+                sum += Integer.parseInt(intArr.get(j).toString());
               }
             }
           }
           // Sum of all integers in the numbers array for each JSON document
-          System.out.println(": Sum of numbers = " + sum);
+          System.out.print(": Individual sum of numbers = " + sum);
           runningTotal += sum;
+          // Print sum of numbers for all documents
+          System.out.println(": Running Total so far = "+runningTotal);
         }
-        // Print sum of numbers for all documents
-        System.out.println("Running Total = "+runningTotal);
-        
         //close connection
         connection.disconnect();
       }
-    } catch (MalformedURLException e) {
-      System.out.println("The URL is malformed. Please enter a valid url.");
-      e.printStackTrace();
     } catch (ProtocolException e) {
       System.out.println("Please make sure the protocol is correct for the url.");
       e.printStackTrace();
